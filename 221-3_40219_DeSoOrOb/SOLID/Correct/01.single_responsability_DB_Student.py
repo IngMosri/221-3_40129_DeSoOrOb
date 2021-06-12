aaeb318591843cf53b7834c95ed83173e25b19d7
Christian Alejandro Mosri <39068565+IngMosri@users.noreply.github.com>
2021-06-12 12:52:27 -0500

    Delete 221-3_40219_DeSoOrOb/SOLID/Correct directory

--- 221-3_40219_DeSoOrOb/SOLID/Correct/01.single_responsability_DB_Student.py
+++
@@ -1,9 +0,0 @@
-class DB_Student:
-    
-
-    def insert_full_name(self):
-        student.append(self.full_name)
-        
-    def remove_full_name(self):
-        student.remove(self.full_name)
-student =[]
