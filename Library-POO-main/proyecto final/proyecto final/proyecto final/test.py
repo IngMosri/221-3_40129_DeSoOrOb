755cdac28975fc3842acd5ae62616f9bebea0724
Christian Alejandro Mosri <39068565+IngMosri@users.noreply.github.com>
2021-07-29 16:42:10 -0500

    Update test.py

--- Library-POO-main/proyecto final/proyecto final/proyecto final/test.py
+++ Library-POO-main/proyecto final/proyecto final/proyecto final/test.py
@@ -1,26 +1,28 @@
-# with is like your try .. finally block in this case
-fh = open("/Users/christianmosrialatorre/Documents/OneDrive - Instituto Superior Autonomo de Occidente A.C/8 cuatrimestre/USB/ProgramacionOrientadaObjetos/Library-POO-main/books_inventory.txt", "r")
+fh = open("books_inventory.txt", "r")
 word = input("Enter the name of the book : ") 
 s = " "
 count = 1
+data = ""
+book_found = False
 while(s):
     s=fh.readline()
-    l=s.split()
-if word in l:
-    print("book found :", count, ":",s)  
-with open('books_inventory.txt', 'r') as file:
-    # read a list of lines into data
-    data = file.readlines()
-with open('books_inventory.txt', 'r') as file:
-    # read a list of lines into data
-    data = file.readlines()
+    l=s.split("|")
+    print(l)
+    if word in l[0] and book_found == False:
+        print("book found :", count, ":",s)  
+        stock_count= int(l[2])
+        stock_count = stock_count-1
+        l[2] = str(stock_count)
+        print("new Stock ",stock_count)
+        print(("|").join(l))
+    new_line = ("|").join(l)
 
+ 
+    data += new_line
+print("data")
 print(data)
-print("nuevo stock " + data[0])
+fh.close()
 
-# now change the 2nd line, note that you have to add a newline
-data[6] = '2\n'
 
-# and write everything back
-with open('books_inventory.txt', 'w') as file:
+with open("books_inventory.txt", "w") as file:
     file.writelines( data )
